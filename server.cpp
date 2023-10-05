320211ebe401039a4370596f7c11e08f145b1ce0
Wiqiro <william.imbert@utbm.fr>
2023-10-05 22:30:25 +0200

    Fin de la première partie

--- server.cpp
+++ server.cpp
@@ -1,45 +1,57 @@
 #include "server.h"
 
-Server::Server() {
-    name = "unnamed-server";
-}
+Server::Server()
+    : name("unnamed-server"), temperature(0), humidity(0), sound(0), light(0) {}
 
-Server::Server(const Server& server) {
-    data = server.data;
-}
+Server::Server(const Server& server)
+    : name(server.name),
+      temperature(server.temperature),
+      humidity(server.humidity),
+      sound(server.sound),
+      light(server.light) {}
 
-Server::Server(const std::string &name, double data) {
-    this->name = name;
-    this->data = data;
-}
+Server::Server(const std::string& name, double temperature, double humidity,
+               double sound, double light)
+    : name(name),
+      temperature(temperature),
+      humidity(humidity),
+      sound(sound),
+      light(light) {}
 
 Server::~Server() {}
 
-Server& Server::operator=(Server& server) {
-    return server;
+Server& Server::operator=(const Server& server) {
+    name = server.name;
+    temperature = server.temperature;
+    humidity = server.humidity;
+    sound = server.sound;
+    light = server.light;
+    return *this;
 }
 
-
-
-void Server::fileWrite(std::ofstream &file) {
+void Server::fileWrite(std::ofstream& file) {
     if (file.is_open()) {
-        file << data << std::endl;
+        file << "Server name: " << name << std::endl;
+        file << "Temperature: " << temperature << std::endl;
+        file << "Humidity: " << humidity << std::endl;
+        file << "Sound: " << sound << std::endl;
+        file << "Light: " << light << std::endl;
     }
 }
 
 void Server::consoleWrite() {
-    std::cout << data;
+    std::cout << "Server name: " << name << std::endl;
+    std::cout << "Temperature: " << temperature << std::endl;
+    std::cout << "Humidity: " << humidity << std::endl;
+    std::cout << "Sound: " << sound << std::endl;
+    std::cout << "Light: " << light << std::endl;
 }
 
-const std::string& Server::getName() {
-    return name;
-}
-
-const double& Server::getData() {
-    return data;
-}
-
-std::ostream &operator<<(std::ostream &os, const Server &server) {
-    os << "Name: " << server.name << "  Data: " << server.data << "";
+std::ostream& operator<<(std::ostream& os, const Server& server) {
+    os << "Server name: " << server.name << std::endl;
+    os << "Temperature: " << server.temperature << std::endl;
+    os << "Humidity: " << server.humidity << std::endl;
+    os << "Sound: " << server.sound << std::endl;
+    os << "Light: " << server.light << std::endl;
     return os;
 }
--- server.h
+++ server.h
@@ -1,53 +1,50 @@
-#include <string>
-#include <map>
-#include <list>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <string>
 
 class Server {
-private:
+   private:
     std::string name;
-    double data;
+    double temperature, humidity, sound, light;
 
-public:
-/**
- * @brief Default constructor for Server
- */
+   public:
+    /**
+     * @brief Default constructor for Server
+     */
     Server();
 
-/**
- * @brief Copy constructor for Server
- * @param server: Server to copy
- */
+    /**
+     * @brief Copy constructor for Server
+     * @param server: Server to copy
+     */
     Server(const Server &server);
 
-/**
- * @brief Construct a new Server object
- * 
- * @param name: Server's name
- * @param data: Server's data
- */
-    Server(const std::string &name, double data);
+    /**
+     * @brief Construct a new Server object
+     *
+     * @param name: Server's name
+     * @param data: Server's data
+     */
+    Server(const std::string &name, double temperature, double humidity,
+           double sound, double light);
 
     ~Server();
 
-    Server &operator=(Server &server);
+    Server &operator=(const Server &server);
 
-/**
- * @brief Write the server data inside a file
- * 
- * @param file: The file in which the server data should be written
- */
+    /**
+     * @brief Write the server data inside a file
+     *
+     * @param file: The file in which the server data should be written
+     */
     void fileWrite(std::ofstream &file);
 
-/**
- * @brief Outputs the server data in the console
- */
+    /**
+     * @brief Outputs the server data in the console
+     */
     void consoleWrite();
-
-    const std::string &getName();
-    const double &getData();
 
     friend std::ostream &operator<<(std::ostream &os, const Server &server);
 };
-
